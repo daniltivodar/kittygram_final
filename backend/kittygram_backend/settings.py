25ea9547d8a8eb6044bb142e5503a802fe1ee63c
Данил Тиводар <danilkativodar@gmail.com>
2023-12-02 23:54:18 +0300

    Change .env file

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -57,8 +57,8 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-	'NAME': os.getenv('POSTGRES_DB', 'sickmoqchima'),
-        'USER': os.getenv('POSTGRES_USER', 'sickmoqchima'),
+	'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432),

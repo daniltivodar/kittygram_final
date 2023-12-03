25b5586e811f2f2b74d98c987fc895efa577b74f
Данил Тиводар <danilkativodar@gmail.com>
2023-12-03 22:44:31 +0300

    Change code after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,10 +1,13 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', '*')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 DEBUG = True
 
@@ -86,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'RU'
 
 USE_I18N = True
 
